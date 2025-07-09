647fd65e2a8104d68078a8136836715f2e08e984
MishaRub1 <misharuban@gmail.com>
2025-07-09 20:57:42 +0300

    implemented classes in presistence file

--- persistence.py
+++ persistence.py
@@ -4,25 +4,38 @@ from dbtools import Dao
  
 # Data Transfer Objects:
 class Employee(object):
-    #TODO: implement
-    pass
+    def __init__(self,id, name , salary, branche):
+        self.id = id
+        self.name = name
+        self.salary = salary
+        self.branche = branche
  
 class Supplier(object):
-    #TODO: implement
-    pass
+    def __init__(self, id, name, contact_information):
+        self.id = id
+        self.name = name
+        self.contact_information = contact_information
 
 class Product(object):
-    #TODO: implement
-    pass
+    def __init__(self, id, description, price, quantity):
+        self.id = id
+        self.description = description
+        self.price = price
+        self.quantity = quantity
 
 class Branche(object):
-    #TODO: implement
-    pass
+    def __init__(self, id, location, number_of_employees):
+        self.id = id
+        self.location = location
+        self.number_of_employees = number_of_employees
 
 class Activitie(object):
-    #TODO: implement
-    pass
- 
+    def __init__(self, product_id, quantity, activator_id, date):
+        self.product_id = product_id
+        self.quantity = quantity
+        self.activator_id = activator_id
+        self.date = date
+
  
 #Repository
 class Repository(object):
